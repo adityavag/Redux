177ac7cd22b6a64bf8d103ea999b73d778b52362
adi247u <adi247u@gmail.com>
2023-09-09 22:59:50 +0530

    combineReducer

--- Redux-Demo/index.js
+++ Redux-Demo/index.js
@@ -1,5 +1,8 @@
 const redux = require('redux');
 const createStore = redux.createStore;
+const combineReducers =  redux.combineReducers;
+
+
 const BUY_CAKE = "BUY_CAKE";
 const BUY_ICE_CREAM = "BUY_ICE_CREAM";
 
@@ -16,17 +19,24 @@ function buyIceCream() {
 }
 
 
-const initialState = {
-    numOfCakes: 10,
+const initialCakeState = {
+    numOfCakes: 10
+}
+const initialIceCreamState = {
     numofIceCreams: 20
 }
 
-const reducer = (state = initialState, action) => {
+const cakeReducer = (state = initialCakeState, action) => {
     switch (action.type) {
         case BUY_CAKE: return {
             ... state,
             numOfCakes : state.numOfCakes-1
         }
+        default: return state
+    }
+}
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+    switch (action.type) {
         case BUY_ICE_CREAM: return {
             ... state,
             numofIceCreams : state.numofIceCreams-1
@@ -35,7 +45,13 @@ const reducer = (state = initialState, action) => {
     }
 }
 
-const store = createStore(reducer);
+
+const rootReducer = combineReducers({
+    cake : cakeReducer,
+    iceCream : iceCreamReducer
+})
+
+const store = createStore(rootReducer);
 console.log("Initial State : ", store.getState())
 
 const unsubscribe =  store.subscribe(()=>console.log("State Change : ", store.getState()))
