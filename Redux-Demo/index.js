3557e3ede234d201eb1597406e74e4f0d3d61544
adi247u <adi247u@gmail.com>
2023-09-09 22:52:59 +0530

    Adding IceCreams

--- Redux-Demo/index.js
+++ Redux-Demo/index.js
@@ -1,6 +1,7 @@
 const redux = require('redux');
 const createStore = redux.createStore;
 const BUY_CAKE = "BUY_CAKE";
+const BUY_ICE_CREAM = "BUY_ICE_CREAM";
 
 function buyCake() {
     return {
@@ -8,9 +9,16 @@ function buyCake() {
     }
 }
 
+function buyIceCream() {
+    return {
+        type: BUY_ICE_CREAM
+    }
+}
+
 
 const initialState = {
-    numOfCakes: 10
+    numOfCakes: 10,
+    numofIceCreams: 20
 }
 
 const reducer = (state = initialState, action) => {
@@ -19,6 +27,10 @@ const reducer = (state = initialState, action) => {
             ... state,
             numOfCakes : state.numOfCakes-1
         }
+        case BUY_ICE_CREAM: return {
+            ... state,
+            numofIceCreams : state.numofIceCreams-1
+        }
         default: return state
     }
 }
@@ -26,9 +38,8 @@ const reducer = (state = initialState, action) => {
 const store = createStore(reducer);
 console.log("Initial State : ", store.getState())
 
-const unsubscribe =  store.subscribe(()=>console.log("State Change"))
+const unsubscribe =  store.subscribe(()=>console.log("State Change : ", store.getState()))
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIceCream())
 unsubscribe()
-store.dispatch(buyCake())
-console.log("Initial State : ", store.getState())
